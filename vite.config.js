9afc99541d3fdb7f2baf60a8b29e61ec09cc6486
Ricky Harerri <ricky_4567@yahoo.co.in>
2026-05-20 12:37:27 -0600

    Fix build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),viteStaticCopy({
+      targets: [
+        { src: 'config', dest: './' },
+        { src: 'public/project_assets', dest: './' }
+      ]
+    })],
   server: {
     historyApiFallback: {
       rewrites: [
